1c6235cdf0c484992ec47eb68a7051e00092a85b
Sergey Gurman <sgurman753@gmail.com>
2020-07-13 14:34:43 -0400

    finished stage2

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,10 +1,33 @@
+from random import choice
+
+
 class Game:
     def __init__(self):
-        option = input()
-        loses_from = {'scissors': 'rock', 'paper': 'scissors', 'rock': 'paper'}
-        end_text = "Sorry, but computer chose {}"
-        print(end_text.format(loses_from.get(option)))
+        self.player_option = input()
+        self.options = ['rock', 'scissors', 'paper']
+        self.computer_option = choice(self.options)
+        self.lose_text = "Sorry, but computer chose {}"
+        self.draw_text = "There is a draw ({})"
+        self.win_text = "Well done. Computer chose {} and failed"
+
+    def run(self):
+        self.compare_result()
+
+    def compare_result(self):
+        player = self.player_option
+        computer = self.computer_option
+        compare_results = (player, computer)
+        win_results = [('rock', 'scissors'), ('scissors', 'paper'), ('paper', 'rock')]
+        loss_results = [('rock', 'paper'), ('scissors', 'rock'), ('paper', 'scissors')]
+
+        if compare_results in loss_results:
+            print(self.lose_text.format(computer))
+        if compare_results in win_results:
+            print(self.win_text.format(computer))
+        if player == computer:
+            print(self.draw_text.format(computer))
 
 
 if __name__ == "__main__":
-    Game()
+    r = Game()
+    r.run()
