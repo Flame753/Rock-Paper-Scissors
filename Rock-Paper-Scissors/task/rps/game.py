60c68bd5aa3095141023da8ea2e3cb89c8bc3bb3
Sergey Gurman <sgurman753@gmail.com>
2020-07-13 13:32:50 -0400

    finished stage1

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,1 +1,10 @@
-# Write your code here
+class Game:
+    def __init__(self):
+        option = input()
+        loses_from = {'scissors': 'rock', 'paper': 'scissors', 'rock': 'paper'}
+        end_text = "Sorry, but computer chose {}"
+        print(end_text.format(loses_from.get(option)))
+
+
+if __name__ == "__main__":
+    Game()
