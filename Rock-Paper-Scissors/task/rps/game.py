52ac719791f845c9efa3b611edef6d41dfbe007b
Sergey Gurman <sgurman753@gmail.com>
2020-07-13 14:54:23 -0400

    update formate

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -4,28 +4,19 @@ from random import choice
 class Game:
     def __init__(self):
         self.player_option = input()
-        self.options = ['rock', 'scissors', 'paper']
-        self.computer_option = choice(self.options)
-        self.lose_text = "Sorry, but computer chose {}"
-        self.draw_text = "There is a draw ({})"
-        self.win_text = "Well done. Computer chose {} and failed"
+        self.computer_option = choice(['rock', 'scissors', 'paper'])
 
     def run(self):
-        self.compare_result()
-
-    def compare_result(self):
         player = self.player_option
         computer = self.computer_option
-        compare_results = (player, computer)
-        win_results = [('rock', 'scissors'), ('scissors', 'paper'), ('paper', 'rock')]
-        loss_results = [('rock', 'paper'), ('scissors', 'rock'), ('paper', 'scissors')]
+        condition = {"rock": "scissors", "scissors": "paper", "paper": "rock"}
 
-        if compare_results in loss_results:
-            print(self.lose_text.format(computer))
-        if compare_results in win_results:
-            print(self.win_text.format(computer))
         if player == computer:
-            print(self.draw_text.format(computer))
+            print(f"There is a draw ({computer})")
+        elif computer == condition[player]:
+            print(f"Well done. Computer chose {computer} and failed")
+        else:
+            print(f"Sorry, but computer chose {computer}")
 
 
 if __name__ == "__main__":
