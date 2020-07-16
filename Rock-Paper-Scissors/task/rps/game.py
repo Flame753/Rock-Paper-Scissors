dba1840acaf0c23e31a281a7c2a46a1b853e4dcf
Sergey Gurman <sgurman753@gmail.com>
2020-07-15 22:57:36 -0400

    finished stage3

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -3,20 +3,37 @@ from random import choice
 
 class Game:
     def __init__(self):
-        self.player_option = input()
-        self.computer_option = choice(['rock', 'scissors', 'paper'])
+        self.player_option = None
+        self.computer_option = None
+        self.running = True
 
     def run(self):
-        player = self.player_option
-        computer = self.computer_option
+        while self.running:
+            self.player_option = input()
+            self.computer_option = choice(['rock', 'scissors', 'paper'])
+            if self.valid_option():
+                self.result()
+
+    def result(self):
         condition = {"rock": "scissors", "scissors": "paper", "paper": "rock"}
 
-        if player == computer:
-            print(f"There is a draw ({computer})")
-        elif computer == condition[player]:
-            print(f"Well done. Computer chose {computer} and failed")
+        if self.player_option == "!exit":
+            print("Bye!")
+            self.running = False
+        elif self.player_option == self.computer_option:
+            print(f"There is a draw ({self.computer_option})")
+        elif self.computer_option == condition[self.player_option]:
+            print(f"Well done. Computer chose {self.computer_option} and failed")
         else:
-            print(f"Sorry, but computer chose {computer}")
+            print(f"Sorry, but computer chose {self.computer_option}")
+
+    def valid_option(self):
+        valid_options = ['rock', 'scissors', 'paper', '!exit']
+        if self.player_option in valid_options:
+            return True
+
+        print('Invalid input')
+        return False
 
 
 if __name__ == "__main__":
