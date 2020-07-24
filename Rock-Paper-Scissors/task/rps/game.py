54348bbcf13e434a4ea0e036230813641ac78c07
Sergey Gurman <sgurman753@gmail.com>
2020-07-24 12:22:45 -0400

    added a way to increase the score

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -25,12 +25,22 @@ class Game:
         if self.player_option == "!exit":
             print("Bye!")
             self.running = False
+
+        elif self.player_option == "!rating":
+            print(f'Your rating: {self.score}')
+
         elif self.player_option == self.computer_option:
             print(f"There is a draw ({self.computer_option})")
+            self.score += 50
+
         elif self.computer_option == condition[self.player_option]:
             print(f"Well done. Computer chose {self.computer_option} and failed")
+            self.score += 100
+
         else:
             print(f"Sorry, but computer chose {self.computer_option}")
+
+        self.file_set_score()
 
     def valid_option(self) -> bool:
         valid_options = ['rock', 'scissors', 'paper', '!exit']
@@ -51,10 +61,13 @@ class Game:
         for line in self.file:
             name, score = line.split()
             if name == self.current_player:
-                self.score = score
+                self.score = int(score)
+        self.file.close()
 
     def file_set_score(self):
-        pass
+        self.file = open('rating.txt', 'a')
+        print(self.current_player + str(self.score), file=self.file)
+        self.file.close()
 
 
 if __name__ == "__main__":
