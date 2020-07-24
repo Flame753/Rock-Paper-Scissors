eb4454dce35f3fea9cc9099b68e826280a4fb0a7
Sergey Gurman <sgurman753@gmail.com>
2020-07-24 12:08:42 -0400

    added the necessary methods

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -3,11 +3,16 @@ from random import choice
 
 class Game:
     def __init__(self):
+        self.current_player = None
+        self.score = 0
         self.player_option = None
         self.computer_option = None
         self.running = True
+        self.file = open('rating.txt', 'w')
+        self.file.close()
 
     def run(self):
+        self.greeting()
         while self.running:
             self.player_option = input()
             self.computer_option = choice(['rock', 'scissors', 'paper'])
@@ -27,13 +32,29 @@ class Game:
         else:
             print(f"Sorry, but computer chose {self.computer_option}")
 
-    def valid_option(self):
+    def valid_option(self) -> bool:
         valid_options = ['rock', 'scissors', 'paper', '!exit']
         if self.player_option in valid_options:
             return True
 
         print('Invalid input')
         return False
+
+    def greeting(self):
+        name = input('Enter your name: ')
+        print('Hello, ' + name)
+        self.current_player = name
+        self.file_get_score()
+
+    def file_get_score(self):
+        self.file = open('rating.txt', 'r')
+        for line in self.file:
+            name, score = line.split()
+            if name == self.current_player:
+                self.score = score
+
+    def file_set_score(self):
+        pass
 
 
 if __name__ == "__main__":
